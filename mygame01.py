629087640702818ab0ca81f2d9754f8549488c3f
DennisTrujilloDev <dennis.joel.trujillo@gmail.com>
2022-09-28 02:54:22 +0000

    proj1 and proj2 completed

--- mygame01.py
+++ mygame01.py
@@ -2,13 +2,16 @@
 """Driving a simple game framework with
    a dictionary object | Alta3 Research"""
 
+#import third party modules
 import random 
 import crayons
 
-def showInstructions():
-    """Show the game instructions when called"""
-    #print a main menu and the commands
-    print('''
+def main():
+    def showInstructions():
+        """Show the game instructions when called"""
+    
+        #print a main menu, commands, context and instructions
+        print('''
     
 
     ========The Haunted Hotel========
@@ -54,44 +57,56 @@ def showInstructions():
    
    -------------------------------------------
     '''
-    )
+        )
 
-def showStatus():
-    """determine the current status of the player"""
-    # print the player's current location
-    print(crayons.blue('******************************'))
-    print('You are in the following room: ' + crayons.green(currentRoom))
-    if 'entity' in rooms[currentRoom]:
-        print(crayons.red(rooms[currentRoom]['entity'] + ': "RAWR!"\nQuick -- you MUST hide (command: hide now)\nbefore moving on to the next room!'))
-    else:
-        print(crayons.cyan('"Hmmm.. it seems the room is empty."'))
-        print(crayons.yellow('Possible direction commands:'))
-        if 'east' in rooms[currentRoom]:
-            print(crayons.yellow('    go east'))
-        elif 'north' in rooms[currentRoom]:
-            print(crayons.yellow('    go north'))
-        elif 'west' in rooms[currentRoom]:
-            print(crayons.yellow('    go west'))
-        elif 'south' in rooms[currentRoom]:
-            print(crayons.yellow('    go south\ln'))
-    print(crayons.green('\nRemaining...'))
-    print('Live(s):', health-1)
-    print('Hide command(s):', hidecount)
-    print('Ouija session(s):', ouijacount)
-    print(crayons.blue('******************************'))
-    print()
+    def showStatus():
+        """determine the current status of the player"""
+    
+        # print the player's current location
+        print(crayons.blue('******************************'))
+        print('You are in the following room: ' + crayons.green(currentRoom))
+    
+        # states it if there's an entity in the room 
+        if 'entity' in rooms[currentRoom]:
+            print(crayons.red(rooms[currentRoom]['entity'] + ': "RAWR!"\nQuick -- you MUST hide (command: hide now)\nbefore moving on to the next room!'))
+        # if there isn't an entity in the room 
+        else:
+            print(crayons.cyan('"Hmmm.. it seems the room is empty."'))
+            print(crayons.yellow('Possible direction commands:'))
+            # if east is in the available directions for that room  
+            if 'east' in rooms[currentRoom]:
+                print(crayons.yellow('    go east'))
+            #if north is in the available directions for that room  
+            elif 'north' in rooms[currentRoom]:
+                print(crayons.yellow('    go north'))
+            # if west is in the available directions for that room 
+            elif 'west' in rooms[currentRoom]:
+                print(crayons.yellow('    go west'))
+            #if south is in the available directions for that room 
+            elif 'south' in rooms[currentRoom]:
+                print(crayons.yellow('    go south\ln'))
+    
+        # print the number of lives and moves available 
+        print(crayons.green('\nRemaining...'))
+        print('Live(s):', health-1)
+        print('Hide command(s):', hidecount)
+        print('Ouija session(s):', ouijacount)
+        print(crayons.blue('******************************'))
+        print()
 
-#COUNTERS
-ouijacount = 2
-hidecount = 4
-health = 2
-currentRoom = 'Lobby'
+    # these counters are used to keep track of how many moves/lives the user has remaining
+    ouijacount = 2
+    hidecount = 4
+    health = 2
 
-#invoke function that shows instructions
-showInstructions()
+    # the variable is set to Lobby in order to start the user off in the lobby
+    currentRoom = 'Lobby'
 
-# dictionary linking a room to other 
-rooms = {
+    #invoke function that shows instructions
+    showInstructions()
+
+    # dictionary linking rooms together and placing entities in rooms 
+    rooms = {
         
             'Lobby' : {
                 'east' : 'Room 113',
@@ -134,79 +149,100 @@ rooms = {
             },
             "Backyard": { 
             },
-    }
+        }
 
-# breaking this while loop means the game is over
-while True:
-    showStatus()
+    # breaking this while loop means the game is over.
+    while True:
+        #invokes function that shows status 
+        showStatus()
 
-    # the player MUST type something in
-    # otherwise input will keep asking
-    move = ''
-    while move == '':  
-        move = input('>').strip()
+        # The player MUST type something in. Otherwise, input will keep asking for user input. The strip method is used here to trim spaces from user input
+        move = ''
+        while move == '':  
+            move = input('>').strip()
 
-    # normalizing input:
-    # .lower() makes it lower case, .split() turns it to a list
-    # therefore, "get golden key" becomes ["get", "golden key"]          
-    move = move.lower().split(" ", 1)
+        # normalizing input:
+        # .lower() makes it lower case, .split() turns it to a list
+        # therefore, "get golden key" becomes ["get", "golden key"]          
+        move = move.lower().split(" ", 1)
 
-    #if they type 'go' first
-    if move[0] == 'go':
-        if move[1] not in rooms[currentRoom]:
-            print("Sorry, there aren't any rooms\nin that direction. Please ensure\nyou have avoided any entities in the\nroom (if any) before exiting.")
-        #check that they are allowed wherever they want to go
-        elif move[1] in rooms[currentRoom] and "entity" not in rooms[currentRoom]:
-            #set the current room to the new room
-            currentRoom = rooms[currentRoom][move[1]]
-        elif move[1] in rooms[currentRoom] and "entity" in rooms[currentRoom]:
-            print("Please try to avoid the dark entity\n(command: hide now) before continuing\non to the next room.")
-            
-    #if they type 'play' first
-    if move[0] == 'play' and move[1] == 'ouija' and ouijacount <= 2 :
-        ouijacount -= 1
-        # 1. Ask user what they'd like to say to the entities
-        input("What would you like to say to the entities?")
-        # 2. play round of ouija (more or less health)
-        entity = random.randint(0, 10)
-        user = random.randint(0, 10)
-        if user >= entity:
-            print("You have distracted them!\nYou've gained 1 life.")
-            health += 1
-        else:
-            print("You're upsetting them angry!\nRAWR RAWR\nYou've lost 2 lives.")
-            health -= 2
-    elif move[0] == 'play' and move[1] == 'ouija' and ouijacount >= 2 :
-        print("Sorry, you've run out of ouija rounds...")
-
-
-    if 'entity' not in rooms[currentRoom] and move[0] == 'hide':
-        print("There either arent any dark entities\nin here or you've already avoided them.\nMove along, now.")
-
-## If a player enters a room with an entity
-    if 'entity' in rooms[currentRoom]:
-      #  print("There's something in here... you must hide!")
-        #NEW: if user inputs flee
-        #NEW: play a round of flee
-        if move[0] == 'hide' and move[1] == 'now' and hidecount <= 4 :
-            hidecount -= 1
+        # if they type 'go' first
+        if move[0] == 'go':
+            # if the user has typed go first and the second word (direction) is not in the list of directions available for that room... 
+            if move[1] not in rooms[currentRoom]:
+                print("Sorry, youre input is invalid.\nPlease ensure\nyou have avoided any entities in the\nroom (if any) before exiting.")
+            # if the user inputs a direction that is valid and the entity is not present in the room...
+            elif move[1] in rooms[currentRoom] and "entity" not in rooms[currentRoom]:
+                # the current room variable is reassigned to the new room
+                currentRoom = rooms[currentRoom][move[1]]
+            # if the user inputs a valid direction but the entity hasn't been avoided
+            elif move[1] in rooms[currentRoom] and "entity" in rooms[currentRoom]:
+                print("Please try to avoid the dark entity\n(command: hide now) before continuing\non to the next room.")
+        #if they type 'play' then 'ouija' and they have available moves for playing ouija
+        if move[0] == 'play' and move[1] == 'ouija' and ouijacount <= 2 :
+            # the number of ouija moves available is decreased by one
+            ouijacount -= 1
+            # ask user what they'd like to communicate to the entities
+            input("What would you like to say to the entities?\n")
+            # play a round of ouija using randint to generate two random integer values
             entity = random.randint(0, 10)
             user = random.randint(0, 10)
+            # if the user's random int is greater than or equal to the entity's
             if user >= entity:
-                print("You hid from the entity -- good going!")
-                #delete entity
-                del rooms[currentRoom]['entity']
+                print("You have distracted them!\nYou've gained 1 life.")
+                #the user gains a life
+                health += 1
+            # if the user's random int is less than or equal to the entity's
             else:
-                print("Ouch, that one hurt!")
-                health -= 1
+                print("You're upsetting them angry!\nRAWR RAWR\nYou've lost 2 lives.")
+                # the user loses 2 lives
+                health -= 2
+        # if the user wants to play ouija but they have run out of ouija moves
+        elif move[0] == 'play' and move[1] == 'ouija' and ouijacount >= 2 :
+            print("Sorry, you've run out of ouija rounds...")
+
+        # if there is no entity in the room but the user chose to try to hide from an entity
+        if 'entity' not in rooms[currentRoom] and move[0] == 'hide':
+            print("There either arent any dark entities\nin here or you've already avoided them.\nMove along, now.")
+
+    # If a player enters a room with an entity
+        if 'entity' in rooms[currentRoom]:
+            # if the user tries to hide and they have moves in order to be able to do so
+            if move[0] == 'hide' and move[1] == 'now' and hidecount <= 4 :
+                # the number of hide oves available decreases by 1 
+                hidecount -= 1
+                # randint is used to generate two random values
+                entity = random.randint(0, 10)
+                user = random.randint(0, 10)
+                # if the user's random int is greater than or equal to the entity's
+                if user >= entity:
+                    print("You hid from the entity -- good going!")
+                # if the user's random int is less than or equal to the entity's
+                else:
+                    print("Ouch, that one hurt!")
+                    # user's health decreases by 1
+                    health -= 1
+                # the entity is removed from the dictionary regardless of whether they have successfully avoided the entity or not 
                 del rooms[currentRoom]['entity']
-        elif hidecount <= 0 and ouijacount <= 0 :
-            print("GAME OVER. Evil got it's way...")
+       
+       # if entity is present but the user doesn't have any ouija or hide moves left 
+            elif hidecount <= 0 and ouijacount <= 0 :
+                print("GAME OVER. Evil got it's way...")
+                # the while loop is broken and the game ends
+                break
+    
+        # if the user has run out of lives
+        if health <= 0:
+            print("0 Lives remaining. You're stuck\nhere forever. Goodbye!")
+            # the game ends via breaking the while loop
             break
-    if health <= 0:
-        print("0 Lives remaining. You're stuck\nhere forever. Goodbye!")
-        break
-    ## Define how a player can win
-    if currentRoom == 'Backyard' and health >= 0:
-        print('Phew... that was close! Congratulations --\nyou made it out of the Haunted Hotel!')
-        break
+    
+        #if the user has made it to the backyard, they have won
+        if currentRoom == 'Backyard':
+            print('Phew... that was close! Congratulations --\nyou made it out of the Haunted Hotel!')
+            #the while loop breaks, ending the game
+            break
+
+# calling main() using this technique allows others to import your code: 
+if __name__ == "__main__":
+    main()
--- project-one/main.py
+++ project-one/main.py
@@ -1,95 +1,123 @@
 #!/usr/bin/env python3
-"""COMMENT HERE"""
-print("'What Metropolises Suits Your Lifestyle?'")
-user = input("To get started, please enter your name: \n")
+"""Alta3 Research | game using python and if logic"""
 
-#COUNTERS
-counter_LA = 0
-counter_Mumbai = 0 
-counter_NYC = 0
-counter_Berlin = 0 
-counter_CDMX = 0
+def main():
+    """the main function holds all of our code and is invoked at runtime"""
+   
+   # prints the title of the game 
+    print("\n****************************************")
+    print("What Metropolises Suits Your Lifestyle?")
+    print("****************************************\n")
+    
+    # holds user's input in variable user 
+    user = input("To get started, please enter your name: \n")
 
-#WEATHER
-weather_pref = ["h", "w", "c", "v"]
-weather = input(f"{user}, what's your ideal weather? Enter h for hot, w for warm, c for chilly/cold, or v for varied.\n").lower()
+    # The counters are used to keep track of what cities are accruing the most points
+    counter_la = 0
+    counter_mumbai = 0 
+    counter_nyc = 0
+    counter_berlin = 0 
+    counter_cdmx = 0
+    
+    # this while loop allows the question to be repeatedly asked until user inputs valid information
+    while True:
+        # the variable weather is assigned to a lowercase version of the user's input...
+        weather = input(f"\n{user}, what's your ideal weather?\nEnter h for hot, w for warm, c for chilly/cold, or v for varied.\n").lower()
+        # as long as it falls under one of the conditions stated below. The while loop breaks once one of these conditions is met and the appropriate variable is updated
+        if weather == "h":
+            counter_mumbai += 1
+            break
+        if weather == "w":
+            counter_cdmx += 1
+            counter_la += 1
+            break
+        if weather == "c":
+            counter_berlin += 1
+            break
+        if weather == "v":
+            counter_nyc += 1
+            break
 
-while True:
-    if weather not in weather_pref:
-        weather = input(f"{user}, what's your ideal weather? Enter h for hot, w for warm, c for chilly/cold, or v for varied.\n").lower()
-    elif weather == "h":
-        counter_Mumbai += 1
-        break
-    elif weather == "w":
-        counter_CDMX += 1
-        counter_LA += 1
-        break
-    elif weather == "c":
-        counter_Berlin += 1
-        break
-    elif weather == "v":
-        counter_NYC += 1
-        break
+    # this while loop allows the question to be repeatedly asked until user inputs a valid response regarding their dietary limitations
+    while True:
+        # the variable diet is assigned to a lowercase version of the user's input...
+        diet = input("\nAre you vegetarian or vegan?\nIf yes, enter y. If no, enter n \n").lower()
+        # as long as it falls under one of the conditions stated below. The while loop breaks once one of these conditions is met and the appropriate variable is updated
+        if diet == "y":
+            counter_mumbai += 1
+            counter_berlin += 1
+            counter_la += 1
+            counter_nyc += 1
+            break
+        if diet == "n":
+            counter_cdmx += 1
+            break
 
-#DIET
-diet = input("Are you vegetarian or vegan? If yes, enter y. If no, enter n \n").lower()
-diet_validation = ["y", "n"]
-while True:
-    if diet not in diet_validation:
-        diet = input("Are you vegetarian or vegan? If yes, enter y. If no, enter n \n").lower()
-    elif diet == "y":
-        counter_Mumbai += 1
-        counter_Berlin += 1
-        counter_LA += 1
-        counter_NYC += 1
-        break
-    elif diet == "n":
-        counter_CDMX += 1
-        break
+    # this while loop allows the question to be repeatedly asked until user inputs a valid response regarding their income
+    while True:
+         # the variable highincome is assigned to a lowercase version of the user's input...
+        highincome = input("\nIs your salary high?\nIf yes, enter y. If no, enter n \n").lower()
+        # as long as it falls under one of the conditions stated below. Thewhile loop breaks once one of these conditions is met and the appropriate variable is updated
+        if highincome == "y":
+            counter_mumbai += 1
+            counter_berlin += 1
+            counter_la += 1
+            counter_nyc += 1
+            counter_cdmx += 1
+            break
+        if highincome == "n":
+            counter_cdmx += 1
+            counter_berlin += 1
+            counter_mumbai += 1
+            break
 
-#INCOME
-highincome = input("Is your salary high? If yes, enter y. If no, enter n \n").lower()
-income_validation = ["y", "n"]
-while True:
-    if highincome not in income_validation:
-        highincome = input("Is your salary high? If yes, enter y. If no, enter n \n").lower()
-    elif highincome == "y":
-        counter_Mumbai += 1
-        counter_Berlin += 1
-        counter_LA += 1
-        counter_NYC += 1
-        counter_CDMX += 1
-        break
-    elif highincome == "n":
-        counter_CDMX += 1
-        counter_Berlin += 1
-        counter_Mumbai += 1
-        break
+    # this while loop allows the question to be repeatedly asked until user inputs a valid response regarding their preferred mode(s) of transportation
+    while True:
+        # the variable trans is assigned to a lowercase version of the user's input...
+        trans = input("\nHow do you prefer to get around?\nIf by car, enter c. If you prefer public transportation, enter p. \n").lower()
+        # as long as it falls under one of the conditions stated below. Thewhile loop breaks once one of these conditions is met and the appropriate variable is updated
+        if trans == "c":
+            counter_mumbai += 1
+            counter_la += 1
+            counter_cdmx += 1
+            break
+        if trans == "p":
+            counter_berlin += 1
+            counter_nyc += 1
+            break
 
-#TRANSPORTATION
-trans = input("How do you prefer to get around? If by car, enter c. If you prefer public transportation, enter p \n").lower()
-trans_validation = ["c", "p"]
-while True:
-    if trans not in trans_validation:
-        trans = input("How do you prefer to get around? If by car, enter c. If you prefer public transportation, enter p. \n").lower()
-    elif trans == "c":
-        counter_Mumbai += 1
-        counter_LA += 1
-        counter_CDMX += 1
-        break
-    elif trans == "p":
-        counter_Berlin += 1
-        counter_NYC += 1
-        break
+    # this dictionary holds the city names and their respective counter 
+    cities = [
+            {
+                "name": "Mumbai",
+                "counter": counter_mumbai
+                }, 
+            {
+                "name": "NYC",
+                "counter": counter_nyc
+                },
+            {
+                "name": "LA",
+                "counter": counter_la
+                },
+            {
+                "name": "Mexico City",
+                "counter": counter_cdmx
+                },
+            {
+                "name": "Berlin",
+                "counter": counter_berlin
+                }
+    ]
 
-#results
-cities = [{"name": "Mumbai", "counter": counter_Mumbai}, {"name": "NYC", "counter": counter_NYC}, {"name": "LA", "counter": counter_LA},{"name": "Mexico City", "counter": counter_CDMX}, {"name": "Berlin", "counter": counter_Berlin}]
-sorted_cities = sorted(cities, key=lambda d: d["counter"])
-#above line
-print(sorted_cities)
-print(f"A couple cities {user} would enjoy:\
-{sorted_cities[-1]['name']}\
-& {sorted_cities[-2]['name']}\
-Time to book those flights!\
-For deals check out https://matrix.itasoftware.com/search\
-Thank you for stopping by!")
+    # the counters' values are sorted using sorted() below, where the key argument is used to sort a more complex list by defining city["counter"] as the values that should be used to sort the cities dictionary. Using the keyword lambda saves us time and space.  
+    sorted_cities = sorted(cities, key=lambda city: city["counter"])
+
+    # the results from the game are printed using the last two cities in the sorted list.
+    print("\n**************************************")
+    print(f"A couple cities {user} would enjoy:\n\n{sorted_cities[-1]['name']} & {sorted_cities[-2]['name']}\n\nTime to book those flights!\nFor deals check out\nhttps://matrix.itasoftware.com/search\nThank you for stopping by!")
+    print("**************************************\n")
+
+#calling main() using this technique allows others to import your code: 
+if __name__ == "__main__":
+    main()
